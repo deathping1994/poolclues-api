d0836acf8437dd82b739d7d90e02943ae810c013
Gaurav Shukla <gshukla66@gmail.com>
2015-09-28 18:43:22 +0530

    test and fail api added for fun

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from flask import jsonify
 from flask import Flask, request, flash, url_for, redirect, \
      render_template, abort, send_from_directory
 
@@ -10,13 +11,20 @@ app.config.from_pyfile('flaskapp.cfg')
 def index():
     return render_template('index.html')
 
+@app.route('/test')
+def test():
+    return jsonify(success="Congrats you are ready to rock!"),200
+
+
+@app.route('/fail')
+def fail():
+    return jsonify(error="You can read the error"),500
+
+
 @app.route('/<path:resource>')
 def serveStaticResource(resource):
     return send_from_directory('static/', resource)
 
-@app.route("/test")
-def test():
-    return "<strong>It's Alive!</strong>"
 
 if __name__ == '__main__':
     app.run()
