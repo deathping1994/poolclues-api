9e00ed6ba6d4b2017d4e2fe4fea6f2c77d664dfa
Gaurav Shukla <gshukla66@gmail.com>
2015-10-04 20:14:31 +0530

    MOdified user model

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ from flask import jsonify
 from flask import Flask,request
 from flask.ext.bcrypt import Bcrypt
 from flask.ext.sqlalchemy import SQLAlchemy
-import models
+# import models
 from flask.ext.pymongo import PyMongo
 import sqlalchemy.exc
 from flask.ext.cors import CORS,cross_origin
@@ -16,6 +16,34 @@ mongo = PyMongo(app)
 db = SQLAlchemy(app)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+class User(db.Model):
+    email_id= db.Column(db.String(80), unique=True,primary_key=True,nullable=False)
+    _password= db.Column(db.String(100),nullable=False)
+    first_name=db.Column(db.String(20),nullable=False)
+    middle_name= db.Column(db.String(20),nullable=True)
+    last_name = db.Column(db.String(20),nullable=True)
+    house_number=db.Column(db.String(40),nullable=True)
+    street=db.Column(db.String(40),nullable=True)
+    city= db.Column(db.String(20),nullable=True)
+    state=db.Column(db.String(20),nullable=True)
+    country=db.Column(db.String(20),nullable=False)
+    verified=db.Column(db.Boolean,default=False,nullable=False)
+
+    def __init__(self, first_name,middle_name,last_name,email_id,_password,house_number,street,city,state,country):
+        self.first_name = first_name
+        self.middle_name=middle_name
+        self.last_name=last_name
+        self.email_id = email_id
+        self._password=_password
+        self.house_number=house_number
+        self.street=street
+        self.city=city
+        self.state=state
+        self.country=country
+        self.verified=False
+
 
 
 def log(e):
@@ -33,7 +61,7 @@ def index():
     data=request.get_json(force=True)
     print(data)
     db.create_all()
-    user = models.User(data['first_name'],data['middle_name'],data['last_name'], data['email_id'],
+    user = User(data['first_name'],data['middle_name'],data['last_name'], data['email_id'],
                  bcrypt.generate_password_hash(data['password']),data['house_no'],data['street'],
                  data['city'],data['state'],data['country'])
     # guest = User('guest', 'guest@example.com')
