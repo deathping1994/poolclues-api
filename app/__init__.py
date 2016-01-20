38be68c89066b17676de39f1f50497fc2481959c
Gaurav Shukla <gshukla66@gmail.com>
2016-01-20 17:51:05 +0530

    restarting after long time, added admin interface

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask.ext.bcrypt import Bcrypt
 # from routes import *
+from flask_admin import Admin
 from flask.ext.pymongo import PyMongo
 from flask.ext.cors import CORS
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask("poolclues")
+admin = Admin(app, name='poolclues', template_mode='bootstrap3')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:gaurav@localhost:5432/poolclues'
 bcrypt=Bcrypt(app)
 mongo = PyMongo(app)
